38fec8c4b45033f0d0fab78ce8b1d0b154a7f40e
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2023-04-17 23:04:50 -0500

    Aplicación básica en Express (toma de notas)

--- notes/index.js
+++ notes/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieSession = require("cookie-session");
 const app = express();
 //middlewares
 const logger = (req, res, next) => {
@@ -7,37 +8,35 @@ const logger = (req, res, next) => {
 };
 app.set("view engine", "pug");
 app.set("views", "views");
-app.use(express.urlencoded());
+app.use(
+  cookieSession({
+    secret: "una_cadena_secreta",
+    maxAge: 24 * 60 * 660 * 1000,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(logger);
+//Muestra la lista de notas
 app.get("/", (req, res) => {
-  const name = req.query.name;
-  const age = req.query.age;
-  //res.send(`Hola  ${name},tienes ${age} años`);
-  const notes = ["Nota 1", "Nota 2", "Nota 3"];
+  const notes = req.session.notes || [];
   res.render("index", { notes });
 });
+//Muestra el formulario para crear una nota
 app.get("/notes/new", (req, res) => {
   res.render("new");
 });
+//Permite crear una nota
 app.post("/notes", (req, res) => {
-  console.log(req.body);
+  req.session.id = (req.session.id || 0) + 1;
+  const id = req.session.id;
+  req.session.notes = req.session.notes || [];
+  req.session.notes.push({
+    id: id,
+    title: req.body.title,
+    body: req.body.body,
+  });
   res.redirect("/");
-});
-
-app.get("/users/:name", (req, res) => {
-  const name = req.params.name;
-  res.send(`<h1>Hola ${name} </h1>`);
-});
-
-app.post("/users", (req, res) => {
-  res.status(404);
-  res.set("X-api-version", "2.0");
-  res.send("No se encontro el recurso");
-});
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Algo salio mal");
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
